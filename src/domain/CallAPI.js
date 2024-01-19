334c2aa3512e936a1f82252b3356b419601e014c
iqbalprasettya <muhamadiqbalprasetya89@gmail.com>
2024-01-19 14:59:14 +0700

    detail movie

--- src/domain/CallAPI.js
+++ src/domain/CallAPI.js
@@ -1,22 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
-export const CallAPI = async ({
-    endpoint,
+export const CallAPI = async ({ endpoint, method, headers, params, data, baseUrl }) => {
+  const baseURL = "http://localhost:3000";
+  const option = {
+    baseURL,
+    url: endpoint,
     method,
     headers,
     params,
     data,
-    baseUrl,
-}) => {
-    const baseURL = 'http://localhost:3000'
-    const option = {
-      baseURL,
-      url: endpoint,
-      method,
-      headers,
-      params,
-      data,
-    }
-    const response = await axios(option)
-    return response?.data
-}
+  };
+  const response = await axios(option);
+  return response?.data;
+};
